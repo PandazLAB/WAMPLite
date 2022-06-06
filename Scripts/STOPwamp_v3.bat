73ac38c653028dee6758161eeb9a0803cc8373c5
badLABZ <61927349+badLABZ@users.noreply.github.com>
2022-06-06 21:50:00 +0100

    stop wamp script

---
+++ Scripts/STOPwamp_v3.bat
@@ -0,0 +1,25 @@
+@echo off 
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+echo off
+
+:config
+title WAMPLite V3
+set script-title=WAMPLite V3
+
+@echo off
+@echo,
+NET STOP wampapache64
+NET STOP wampmariadb64
+NET STOP wampmysqld64
+@echo,
+@echo ##################################################################
+@echo,
+@echo   ..... WAMP STOPPED
+@echo,
+@echo ##################################################################
+@echo,
+@echo ...Closing in 1 seconds.
+@echo,
+
+timeout /t 1
+exit
