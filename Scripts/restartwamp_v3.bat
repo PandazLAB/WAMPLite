fe44eab0309fe8072c9c3bb18d9697ba1675ce10
badLABZ <61927349+badLABZ@users.noreply.github.com>
2022-06-06 22:34:15 +0100

    changed title of script

--- Scripts/restartwamp_v3.bat
+++ Scripts/restartwamp_v3.bat
@@ -1,10 +1,10 @@
-@echo off 
+@echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 echo off
 
 :config
-title WAMPLite V3
-set script-title=WAMPLite V3
+title WAMPLite V1
+set script-title=WAMPLite V1
 
 @echo off
 @echo,
